980643774084587eaaf72c80fc367b8737458f7e
Kant <suwatcharin13@hotmail.com>
2022-12-13 17:09:24 +0700

    update: package.json

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ import express from "express";
 
 import uploadRoutes from "./src/routes/upload.route.js";
 
-const app = express();
-const port = 8080;
+function init() {
+  const app = express();
+  const PORT = 8080;
 
-let corsOptions = {
-  origin: `http://localhost:${port}`,
-};
+  let corsOptions = {
+    origin: `http://localhost:${PORT}`,
+  };
 
-app.use(cors(corsOptions));
+  app.use(cors(corsOptions));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: true }));
-uploadRoutes(app);
+  uploadRoutes(app);
 
-app.listen(port, () => {
-  console.log(`Running at localhost:${port}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Running at localhost:${PORT}`);
+  });
+}
+
+init();
